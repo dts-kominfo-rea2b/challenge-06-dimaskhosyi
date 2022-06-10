75732ff922de2c0c2b86cfea0f47fd5d79c16860
Dimas Khosyi Triangga <64787626+dimaskhosyi@users.noreply.github.com>
2022-06-10 23:30:36 +0700

    Done

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs')
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,52 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const newFile1 = (fnCallback) => {
+  fs.readFile(file1, (err, data) => {
+    if(err){
+      return console.log('Ada terjadi error: ', err)
+    }
+    const newData = JSON.parse(data)
+    const processData = newData.message.split(" ")
+    fnCallback(processData[1])
+  })
+}
+
+const newFile2 = (file1, fnCallback) => {
+  fs.readFile(file2, (err, data) => {
+    if(err){
+      return console.log('Ada terjadi error: ', err)
+    }
+    const newData = JSON.parse(data)
+    const processData = newData[0].message.split(" ")
+    const combineData = `${file1} ${processData[1]}`
+    fnCallback(combineData)
+  })
+}
+
+const newFile3 = (file2, fnCallback) => {
+  fs.readFile(file3, (err, data) => {
+    if(err){
+      return console.log('Ada terjadi error: ', err)
+    }
+    const newData = JSON.parse(data)
+    const processData = newData[0].data.message.split(" ")
+    const combineData = `${file2} ${processData[1]}`
+    fnCallback(combineData)
+  })
+}
+
+const bacaData = (fnCallback) => {
+  newFile1(image => {
+    newFile2(image, (image2) => {
+      newFile3(image2, (image3) =>{
+        const data = image3.split(" ")
+        fnCallback(data)
+        return data
+      })
+    })
+  })
+}
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
